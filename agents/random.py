340910da90f0e9eb9c6e9ad8cb3eee3af8bb2fa9
quillaur <aurelienquillet@gmail.com>
2022-03-24 09:13:09 +0100

    Minor change.

--- agents/random.py
+++ agents/random.py
@@ -2,6 +2,6 @@ import chess
 import random
 
 
-def select_move(board: chess.Board, is_white: bool):
+def select_move(board: chess.Board, is_white: bool) -> chess.Move:
 	moves = list(board.legal_moves)
 	return random.choice(moves)
