2cd13cbadf7532cb10a2a11022d880ce48629724
quillaur <aurelienquillet@gmail.com>
2022-03-25 11:28:59 +0100

    Avancé du live du 25032022.

--- main_live.py
+++ main_live.py
@@ -24,16 +24,21 @@ if __name__ == '__main__':
 	if white_name != "None" and black_name != "None":
 		st.header(f"{white_name} (white) versus {black_name} (black)")
 
-		if st.sidebar.button("Play"):
+		play = st.sidebar.button("Play")
+
+		if play:
 
 			white_agent = importlib.import_module(f"agents.{white_name}")
 			black_agent = importlib.import_module(f"agents.{black_name}")
 
 			board = chess.Board()
 
+			turn_container = st.empty()
 			board_image = st.empty()
 
 			is_white_turn = True
+			st.session_state["images"] = []
+			count_turn = 0
 			while True:
 				# Selectionner le coup à jouer
 				if is_white_turn:
@@ -47,24 +52,38 @@ if __name__ == '__main__':
 				# Jouer le coup
 				board.push(move)
 
+				# Afficher le tour
+				turn_container.subheader(f"Turn: {count_turn}")
+				count_turn += 1
+
 				# Visualiser le coup
 				svg_board = chess.svg.board(board)
 
 				my_png = cairosvg.svg2png(bytestring=svg_board.encode('utf-8'), output_width=600, output_height=500)
 				board_image.image(my_png)
+				st.session_state["images"].append(my_png)
 
 
 				if board.outcome():
+					if is_white_turn:
+						if board.is_checkmate():
+							st.session_state["status"] = "White WINS !"
+						else:
+							st.session_state["status"] = "It's a DRAW !"
+					else:
+						if board.is_checkmate():
+							st.session_state["status"] = "Black WINS !"
+						else:
+							st.session_state["status"] = "It's a DRAW !"
 					break
 
+			turn_container.empty()
+			board_image.empty()
 
-		# svr_board = chess.svg.board(board)
+		if "status" in st.session_state:
+			st.header(st.session_state["status"])
 
-		# with open("svg_board.svg", "w") as svg:
-		# 	svg.write(svr_board)
-
-		# legal_moves = list(board.legal_moves)
-
-
-
-		# st.write(legal_moves) 
+		if "images" in st.session_state and st.session_state["images"]:
+			count_turn = len(st.session_state["images"])
+			img_index = st.select_slider("Select a turn:", options=list(range(count_turn)))
+			st.image(st.session_state["images"][img_index])
